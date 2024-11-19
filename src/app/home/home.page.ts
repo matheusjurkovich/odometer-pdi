5f5b8895657c41b71de7703740b1e51d521d6718
matheusjurkovich <matheusjurkovich@gmail.com>
2024-11-19 20:20:46 -0300

    feat: 🎸 add axios and remove ocr

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
-import { Ocr, TextDetections } from '@capacitor-community/image-to-text';
 
 @Component({
   selector: 'app-home',
@@ -8,23 +6,11 @@ import { Ocr, TextDetections } from '@capacitor-community/image-to-text';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  textDetections: any[] = [];
+  public odometer: string = '';
 
   constructor() {}
 
   async scanNow() {
-    const photo = await Camera.getPhoto({
-      quality: 100,
-      allowEditing: false,
-      resultType: CameraResultType.Uri,
-      source: CameraSource.Camera,
-    });
-
-    const data: TextDetections = await Ocr.detectText({
-      filename: photo.path!,
-    });
-
-    this.textDetections = data.textDetections;
+    this.odometer = 'Scanning...';
   }
-
 }
